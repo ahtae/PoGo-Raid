29323d2e11378acec3c713c69520bf1235e93cfd
Kristy Li <kristyli20.0@gmail.com>
2020-12-11 21:27:05 -0500

    feat: Create an Apollo server

---
+++ server.js
@@ -0,0 +1,19 @@
+const { ApolloServer } = require('apollo-server');
+const { sequelize } = require('./models');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: (ctx) => ctx,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+
+  sequelize
+    .authenticate()
+    .then(() => console.log('Database is connected!'))
+    .catch((err) => console.log(err));
+});
