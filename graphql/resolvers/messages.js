aa45af745a75a47fd6d74761d61fdb8531b6e60e
Kristy Li <kristyli20.0@gmail.com>
2020-12-12 19:33:25 -0500

    feat: Create a messages resolver

---
+++ graphql/resolvers/messages.js
@@ -0,0 +1,71 @@
+const { UserInputError, AuthenticationError } = require('apollo-server');
+const { Op } = require('sequelize');
+const { User, Message } = require('../../models');
+
+module.exports = {
+  Query: {
+    getMessages: async (_, { from }, { user }) => {
+      try {
+        if (!user) {
+          throw new AuthenticationError('Unauthenticated!');
+        }
+
+        const otherUser = await User.findOne({ where: { username: from } });
+
+        if (!otherUser) {
+          throw new UserInputError('User not found!');
+        }
+
+        const usernames = [user.username, otherUser.username];
+
+        const messages = await Message.findAll({
+          where: {
+            from: { [Op.in]: usernames },
+            to: { [Op.in]: usernames },
+          },
+          order: [['createdAt', 'DESC']],
+        });
+
+        return messages;
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
+  Mutation: {
+    sendMessage: async (_, { to, content }, { user }) => {
+      try {
+        console.log(user);
+        if (!user) {
+          throw new AuthenticationError('Unauthenticated!');
+        }
+
+        const recipient = await User.findOne({
+          where: {
+            username: to,
+          },
+        });
+
+        if (!recipient) {
+          throw new UserInputError('User is not found!');
+        } else if (recipient.username === user.username) {
+          throw new UserInputError('You cannot message yourself!');
+        }
+
+        if (content.trim() === '') {
+          throw new UserInputError('Message is empty!');
+        }
+
+        const message = await Message.create({
+          to,
+          content,
+          from: user.username,
+        });
+
+        return message;
+      } catch (err) {
+        throw err;
+      }
+    },
+  },
+};
