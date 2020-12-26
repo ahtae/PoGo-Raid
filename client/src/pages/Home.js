7bd5b39408dfc6f1d6061e4401244493db0a8ad0
Kristy Li <kristyli20.0@gmail.com>
2020-12-25 21:11:51 -0500

    feat: Create a Home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Row, Button, Image } from 'react-bootstrap';
+import PokemonGoBanner from '../assets/images/banner.jpg';
+
+const Home = ({ history }) => {
+  const handleNavigateToLogin = () => {
+    history.push('/login');
+  };
+
+  const handleNavigateToRegister = () => {
+    history.push('/register');
+  };
+
+  return (
+    <Row className="bg-white py-5 justify-content-center">
+      <div className="homeContainer">
+        <h1 className="text-center">PoGo Raid</h1>
+        <Image src={PokemonGoBanner} className="homeContainer__image" />
+        <div className="text-center m-3">
+          <Button
+            variant="primary"
+            type="submit"
+            onClick={handleNavigateToLogin}
+          >
+            Login
+          </Button>
+          <Button
+            variant="success"
+            type="submit"
+            onClick={handleNavigateToRegister}
+          >
+            Register
+          </Button>
+        </div>
+      </div>
+    </Row>
+  );
+};
+
+export default Home;
