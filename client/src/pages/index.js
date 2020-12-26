5cda332efda1c58c732451f0e06d9c6ae8f2e808
Kristy Li <kristyli20.0@gmail.com>
2020-12-25 21:12:31 -0500

    chore: Import and export pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import Dashboard from './Dashboard/Dashboard';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+
+export { Dashboard };
+export { Home };
+export { Login };
+export { Register };
