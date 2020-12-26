9899f26c75df8da4c8eedba5dac77b89506a6343
Kristy Li <kristyli20.0@gmail.com>
2020-12-25 21:57:53 -0500

    feat: Create a REGISTER_USER mutation

---
+++ client/src/graphql/mutations/registerUser.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client';
+
+const REGISTER_USER = gql`
+  mutation register(
+    $username: String!
+    $email: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    register(
+      username: $username
+      email: $email
+      password: $password
+      confirmPassword: $confirmPassword
+    ) {
+      username
+      email
+      createdAt
+    }
+  }
+`;
+
+export default REGISTER_USER;
