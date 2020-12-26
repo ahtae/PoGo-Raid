9b248e51cc3f7610dda460ac050660c6742488d3
Kristy Li <kristyli20.0@gmail.com>
2020-12-25 21:55:57 -0500

    refactor: Styling and LOGIN_USER GraphQL mutation

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,9 @@
 import React, { useState } from 'react';
 import { Row, Col, Form, Button, FormText } from 'react-bootstrap';
-import { gql, useLazyQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import { useAuthDispatch } from '../context/auth';
-
-const LOGIN_USER = gql`
-  query login($username: String!, $password: String!) {
-    login(username: $username, password: $password) {
-      username
-      email
-      createdAt
-      token
-    }
-  }
-`;
+import LOGIN_USER from '../graphql/mutations/loginUser';
 
 const Login = ({ history }) => {
   const [username, setUsername] = useState('');
@@ -21,14 +11,15 @@ const Login = ({ history }) => {
   const [errors, setErrors] = useState({});
   const dispatch = useAuthDispatch();
 
-  const [loginUser, { loading }] = useLazyQuery(LOGIN_USER, {
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
     onError: (err) => setErrors(err.graphQLErrors[0].extensions.errors),
     onCompleted: (data) => {
       dispatch({
         type: 'LOGIN',
         payload: data.login,
       });
-      history.push('/');
+
+      history.push('/dashboard');
     },
   });
 
@@ -55,38 +46,40 @@ const Login = ({ history }) => {
 
   return (
     <Row className="bg-white py-5 justify-content-center">
-      <Col sm={8} md={6} lg={4}>
+      <Col sm={8} md={6} lg={4} className="formContainer">
         <h1 className="text-center">Login</h1>
-        <Form onSubmit={handleSubmitClick}>
-          <Form.Group>
+        <Form onSubmit={handleSubmitClick} className="formContainer__form">
+          <Form.Group className="formContainer__form__formGroup">
+            {' '}
             <Form.Label>Username</Form.Label>
             <Form.Control
               type="text"
               value={username}
-              className={errors.username && 'is-invalid'}
+              className={errors && errors.username && 'is-invalid'}
               onChange={handleUsernameChange}
             />
-            <FormText style={{ color: 'red' }}>
-              {errors.username ? errors.username : ''}
+            <FormText className="formContainer__form__formGroup__error">
+              {errors && errors.username ? errors.username : ''}
             </FormText>
           </Form.Group>
-          <Form.Group>
+          <Form.Group className="formContainer__form__formGroup">
+            {' '}
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
-              className={errors.password && 'is-invalid'}
+              className={errors && errors.password && 'is-invalid'}
               value={password}
               onChange={handlePasswordChange}
             />
-            <FormText style={{ color: 'red' }}>
-              {errors.password ? errors.password : ''}
+            <FormText className="formContainer__form__formGroup__error">
+              {errors && errors.password ? errors.password : ''}
             </FormText>
           </Form.Group>
           <div className="text-center">
             <Button variant="success" type="submit" disabled={loading}>
               Login
             </Button>
-            <p style={{ margin: '10px' }}>
+            <p className="py-2">
               Don't have an account? <Link to="/register">Register</Link>
             </p>
           </div>
