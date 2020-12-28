e260cd995b5a6ca2a09f9d2463bbc9cf7f19d8b4
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 22:58:19 -0500

    feat: Create a GET_USERS GraphQL query

---
+++ client/src/graphql/queries/getUsers.js
@@ -0,0 +1,20 @@
+import { gql } from '@apollo/client';
+
+const GET_USERS = gql`
+  query getUsers {
+    getUsers {
+      username
+      createdAt
+      imageUrl
+      latestMessage {
+        uuid
+        from
+        to
+        content
+        createdAt
+      }
+    }
+  }
+`;
+
+export default GET_USERS;
