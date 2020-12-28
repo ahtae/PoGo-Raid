5f163b9aefdfd808573759dd43d9bc40c32e3044
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 22:20:03 -0500

    feat: Create a SEND_MESSAGE GraphQL mutation

---
+++ client/src/graphql/mutations/sendMessage.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+
+const SEND_MESSAGE = gql`
+  mutation sendMessage($to: String!, $content: String!) {
+    sendMessage(to: $to, content: $content) {
+      uuid
+      from
+      to
+      content
+      createdAt
+    }
+  }
+`;
+
+export default SEND_MESSAGE;
