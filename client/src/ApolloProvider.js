1bd61792a0c76d768f28ba2bc0d5b4b6507491bb
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 22:55:10 -0500

    feat: Create Apollo provider

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,61 @@
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  ApolloProvider as Provider,
+  split,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from '@apollo/client/utilities';
+
+let httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+httpLink = authLink.concat(httpLink);
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+const ApolloProvider = (props) => {
+  return <Provider client={client} {...props} />;
+};
+
+export default ApolloProvider;
