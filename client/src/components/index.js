0bf43f214ef25fef2300377c454ffffbdc1d8e1c
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 08:28:12 -0500

    chore: Import and export components

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
 import Messages from './messages/Messages';
-import Users from './Users';
+import Users from './users/Users';
+import DynamicRoute from './DynamicRoute';
 
 export { Messages };
 export { Users };
+export { DynamicRoute };
