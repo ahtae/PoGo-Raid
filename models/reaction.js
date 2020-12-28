7431ea7d01bfb48ea381c0ea431de755fe0a090d
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 22:34:53 -0500

    feat: Create a reactions table

---
+++ models/reaction.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Reaction extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ User, Message }) {
+      this.belongsTo(User, { foreignKey: 'userId' });
+      this.belongsTo(Message, { foreignKey: 'messageId' });
+    }
+  }
+  Reaction.init(
+    {
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      messageId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Reaction',
+      tableName: 'reactions',
+    }
+  );
+  return Reaction;
+};
