e7d8d0760a1d08f5326b55e74907c102769fc75a
Kristy Li <kristyli20.0@gmail.com>
2020-12-27 22:28:51 -0500

    fix: Add a conditional for the token

--- utils/contextMiddleware.js
+++ utils/contextMiddleware.js
@@ -1,14 +1,25 @@
+const { PubSub } = require('apollo-server');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config/env.json');
 
-const contextMiddleware = (context) => {
-  if (context.req && context.req.headers.authorization) {
-    const token = context.req.headers.authorization.split('Bearer ')[1];
+const pubsub = new PubSub();
 
-    jwt.verify(token, JWT_SECRET, (_err, decodedToken) => {
+const contextMiddleware = (context) => {
+  let token;
+
+  if (context.req && context.req.headers.authorization) {
+    token = context.req.headers.authorization.split('Bearer ')[1];
+  } else if (context.connection && context.connection.context.Authorization) {
+    token = context.connection.context.Authorization.split('Bearer ')[1];
+  }
+
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
       context.user = decodedToken;
     });
   }
+
+  context.pubsub = pubsub;
 
   return context;
 };
