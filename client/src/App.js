e2045266ffcead142cb1bed393e7d810ff768bdb
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 00:09:55 -0500

    feat: Add React Router and various providers

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Home, Register, Login, Dashboard } from './pages';
+import ApolloProvider from './ApolloProvider';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { AuthProvider } from './context/auth';
+import { MessageProvider } from './context/message';
+import './App.scss';
+import DynamicRoute from './utils/DynamicRoute';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider>
+      <AuthProvider>
+        <MessageProvider>
+          <BrowserRouter>
+            <Container className="pt-5">
+              <Switch>
+                <DynamicRoute exact path="/" component={Home} />
+                <DynamicRoute
+                  exact
+                  path="/dashboard"
+                  component={Dashboard}
+                  authenticated
+                />
+                <DynamicRoute
+                  exact
+                  path="/register"
+                  component={Register}
+                  guest
+                />
+                <DynamicRoute exact path="/login" component={Login} guest />
+              </Switch>
+            </Container>
+          </BrowserRouter>
+        </MessageProvider>
+      </AuthProvider>
+    </ApolloProvider>
   );
-}
+};
 
 export default App;
