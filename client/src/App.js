43523ac95e1ef86757df3de7db565a16bd89e8e2
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 09:05:15 -0500

    chore: Change import statement

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import ApolloProvider from './ApolloProvider';
 import { BrowserRouter, Switch } from 'react-router-dom';
 import { AuthProvider } from './context/auth';
 import { MessageProvider } from './context/message';
+import { DynamicRoute } from './components';
 import './App.scss';
-import DynamicRoute from './utils/DynamicRoute';
 
 const App = () => {
   return (
