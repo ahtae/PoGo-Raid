ccf4d70c7cd397d99be2e70089e7bb0776d885e0
Kristy Li <kristyli20.0@gmail.com>
2020-12-28 09:05:43 -0500

    chore: Change styling

--- client/src/components/users/Users.js
+++ client/src/components/users/Users.js
@@ -34,12 +34,7 @@ const Users = () => {
   }
 
   return (
-    <Col
-      xs={2}
-      md={4}
-      className="p-0 bg-secondary"
-      style={{ border: '1px solid gray' }}
-    >
+    <Col xs={2} md={4} className="p-0 bg-secondary usersContainer">
       {allUsers}
     </Col>
   );
